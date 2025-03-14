1bd987a086d86377633bc027183d5edaeb945339
ChanDru-Balu <prochandru@gmail.com>
2025-03-14 22:51:51 +0530

    spell check 4

--- index.js
+++ index.js
@@ -1,24 +1,32 @@
 const express = require('express');
 const app = express();
 
-const todoRoutes = require('./routes/todo.routes');
+// const todoRoutes = require('./routes/todo.routes');
 
-const port = 3000 ;
+// const port = 3000 ;
 
-app.use(express.json());
+// app.use(express.json());
 
-app.use('/todo',todoRoutes);
+// app.use('/todo',todoRoutes);
 
-app.get('/',(req,res)=>{
-    res.send('Welcome , But this is the Boiler Plate place only , use propoer routes!')
-});
+// app.get('/',(req,res)=>{
+//     res.send('Welcome , But this is the Boiler Plate place only , use propoer routes!')
+// });
 
-app.use((req,res)=>{
-    res.status(404).send('Path not found!');
-})
+// app.use((req,res)=>{
+//     res.status(404).send('Path not found!');
+// })
 
 // app.listen(port,()=>{
 //     console.log(`Todo app listening on ${port}!`);
 // });
 
-module.exports = app
+const port = 8080;
+
+app.get('/',(req,res)=>{
+    res.send('Welcome!');
+})
+
+app.listen(port,()=>{
+    'Sever started!'
+})
