2f0b35d2ae323aff0e3db2fc8ed4730f1b6c82d3
ChanDru-Balu <prochandru@gmail.com>
2025-03-15 14:11:15 +0530

    cors added

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 const todoRoutes = require('./routes/todo.routes');
 
 const port = 3000 ;
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/todo',todoRoutes);
