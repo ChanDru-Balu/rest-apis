221bf75dfec38f81d56004e703a6333239c4acde
ChanDru-Balu <prochandru@gmail.com>
2025-04-02 23:30:43 +0530

    schema level validation

--- src/auth/user.model.js
+++ src/auth/user.model.js
@@ -8,7 +8,7 @@ const { validate } = require('../todo/todo.model');
 const userSchema = new mongoose.Schema({
     email: {
         type: String,
-        require: true ,
+        // require: true ,
         unique: true ,
         validate: (value)=>{
             const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -16,10 +16,27 @@ const userSchema = new mongoose.Schema({
         },
         message : props => `${props.value} is not a valid email address!`
     } ,
+    mobile: {
+        type: Number,
+        unique: true ,
+    },
+    userName: {
+        type:String,
+        unique: true
+    },
     password: {
         type: String,
         require: true
     }
+});
+
+userSchema.pre('validate', function (next) {
+    if (!this.email && !this.mobile && !this.username) {
+        this.invalidate('email', 'At least one of email or mobile  0r user name  must be provided!');
+        this.invalidate('mobile', 'At least one of email or mobile  0r user name  must be provided!');
+        this.invalidate('userName', 'At least one of email or mobile  0r user name  must be provided!');
+    }
+    next();
 });
 
 const User = mongoose.model('user', userSchema);
